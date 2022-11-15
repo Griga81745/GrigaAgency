a835237ba0c50825782c73aa003022a9ff0c6903
griga <gr_73@inbox.ru>
2022-11-15 09:54:22 +0400

    fix:send form method

--- pages/api/form.js
+++ pages/api/form.js
@@ -4,14 +4,14 @@ export default function handler(req, res) {
     const tgbot = "5702424973:AAF6gxNpnJltiAmCfLUXCpznR81_2-F_yEU"
     var message = JSON.stringify(req.body, null, 2);
 
-    if (req.method === 'POST') {
-        const ret = fetch(
-            `https://api.telegram.org/bot${tgbot}/sendMessage?chat_id=2071502991&text=${message}&parse_mode=HTML`
-        );
-        res.status(200).redirect("/")
-
-    }else{
-        res.status(405).redirect("/")
-    }
+    const ret = fetch(
+        `https://api.telegram.org/bot${tgbot}/sendMessage?chat_id=2071502991&text=${message}&parse_mode=HTML`
+    );
+    // if (req.method === 'POST') {
+    //     res.status(200).redirect("/")
+    //
+    // }else{
+    //     res.status(405).redirect("/")
+    // }
 
 }
